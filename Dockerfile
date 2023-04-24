90a8e3221765a979c3620cfcda59874ba204feaa
Kasdal <plesmilan@hotmail.com>
2023-04-24 11:46:32 +0100

    Fix for increment

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM openjdk:8-jre-alpine
 
+ARG APP_VERSION
+
 EXPOSE 8080
 
-COPY ./target/java-maven-app-*.jar /usr/app/
+COPY ./target/java-maven-app-${APP_VERSION}.jar /usr/app/
 WORKDIR /usr/app
 
-CMD java -jar java-maven-app-*.jar
+CMD java -jar java-maven-app-${APP_VERSION}.jar
--- script.groovy
+++ script.groovy
@@ -7,9 +7,10 @@ def buildDockerImage() {
     echo "build the docker image..."
     echo "Build the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t $USER/myapp:app-1.0 ."
+        def imageName = "$USER/myapp:$env.IMAGE_NAME"
+        sh "docker build --build-arg APP_VERSION=$env.IMAGE_NAME -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push $USER/myapp:app-1.0"
+        sh "docker push $imageName"
     }
 }
 
